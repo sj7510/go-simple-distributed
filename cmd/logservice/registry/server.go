4ecbe738e239dc2281ab939160fc99fe14cb3a73
huajibin <hujiabin@thunisoft.com>
2024-07-28 23:06:59 +0800

    init

---
+++ cmd/logservice/registry/server.go
@@ -0,0 +1,55 @@
+package registry
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"sync"
+)
+
+const ServerPort = ":3000"
+const ServeicesURL = "http://localhost" + ServerPort + "/services"
+
+type registry struct {
+	registrations []Registration
+	mutex         *sync.RWMutex
+}
+
+func (r *registry) add(reg Registration) error {
+	r.mutex.Lock()
+	r.registrations = append(r.registrations, reg)
+	r.mutex.Unlock()
+	return nil
+}
+
+var reg = registry{
+	registrations: make([]Registration, 0),
+	mutex:         new(sync.RWMutex),
+}
+
+type RegistryService struct {
+}
+
+func (s RegistryService) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	log.Println("Request recevied")
+	switch r.Method {
+	case http.MethodPost:
+		dec := json.NewDecoder(r.Body)
+		var r Registration
+		err := dec.Decode(&r)
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		log.Printf("Adding service: %v with URL: %s\n", r.ServiceName, r.ServiceURL)
+		err = reg.add(r)
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
